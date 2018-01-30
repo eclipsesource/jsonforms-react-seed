a87e418984bb7eb113a713d88e0e905e21816673
Edgar Mueller <emueller@eclipsesource.com>
2018-01-30 12:08:45 +0100

    Extract tester to dedicated file

--- src/RatingControl.js
+++ src/RatingControl.js
@@ -1,14 +1,7 @@
 import * as React from 'react';
-import {
-  rankWith,
-  scopeEndsWith,
-  mapStateToControlProps,
-  mapDispatchToControlProps
-} from '@jsonforms/core';
+import { mapStateToControlProps, mapDispatchToControlProps } from '@jsonforms/core';
 import { connect } from 'react-redux';
 import { Rating } from './Rating';
-
-export const ratingControlTester = rankWith(Number.MAX_VALUE, scopeEndsWith('rating'));
 
 const RatingControl = ({ data, handleChange, path }) => (
   <Rating
