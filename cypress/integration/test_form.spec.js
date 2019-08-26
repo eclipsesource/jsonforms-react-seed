c2a15d69a78b58222c26625e5f48757fdbd1707a
Florian Gareis <mail@zoker.me>
2019-08-26 15:42:16 +0200

    Update to 2.3.1-beta.0

--- cypress/integration/test_form.spec.js
+++ cypress/integration/test_form.spec.js
@@ -8,23 +8,18 @@ context('Form', () => {
 
   it('should change input values', () => {
     const nameText = 'John Deer';
-    cy.get('[id="#/properties/name-input"]').clear().type(nameText);
-
     const descText = 'This is a desc test';
+    const recurrenceIntervalText = 10;
+    const dateText = '2001-03-15';
+
+    cy.get('[id="#/properties/name-input"]').clear().type(nameText);
     cy.get('[id="#/properties/description-input"]').clear().type(descText);
-
     cy.get('[id="#/properties/done-input"]').uncheck();
-
     cy.get('[id="#/properties/recurrence"] > div').click();
     cy.get('[data-value="Monthly"]').click();
-
-    const recurrenceIntervalText = 10;
     cy.get('[id="#/properties/recurrence_interval-input"]').clear().type(recurrenceIntervalText);
-
-    cy.get('[id="#/properties/due_date-input"]').clear().type('35');
-
+    cy.get('[id="#/properties/due_date-input"]').clear().type(dateText);
     cy.get('[id="#/properties/rating"] span:last').click();
-
     cy.get('[id="boundData"]').invoke('text').then((content => {
       const data = JSON.parse(content);
 
@@ -37,7 +32,7 @@ context('Form', () => {
       expect(data.done).to.equal(false);
       expect(data.recurrence).to.equal('Monthly');
       expect(data.recurrence_interval).to.equal(recurrenceIntervalText);
-      expect(data.due_date).to.equal('2001-03-15');
+      expect(data.due_date).to.equal(dateText);
       expect(data.rating).to.equal(5);
     }));
   });
@@ -45,11 +40,11 @@ context('Form', () => {
   it('should show errors', () => {
     cy.get('[id="#/properties/name-input"]').clear();
 
-    cy.get('[id="#/properties/name"] p').should('not.be.empty');
+    cy.get('[id="#/properties/name"] p:first-child').should('not.be.empty');
 
     cy.get('[id="#/properties/due_date-input"]').clear().type('351');
 
-    cy.get('[id="#/properties/due_date"] p').should('not.be.empty');
+    cy.get('[id="#/properties/due_date"] p:first-child').should('not.be.empty');
 
     cy.get('[id="#/properties/recurrence"] > div').click();
     cy.get('[data-value="Never"]').click();
