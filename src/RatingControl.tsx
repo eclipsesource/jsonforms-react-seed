d79417f184efcfa2f978fe7c7787bf9972fee4ff
LukasBoll <ga24xeg@mytum.de>
2022-04-26 00:29:53 +0200

    add i18n
    Signed-off-by: Lukas Boll lukas-bool@web.de

--- src/RatingControl.tsx
+++ src/RatingControl.tsx
@@ -5,12 +5,14 @@ interface RatingControlProps {
   data: any;
   handleChange(path: string, value: any): void;
   path: string;
+  label: string;
 }
 
-const RatingControl = ({ data, handleChange, path }: RatingControlProps) => (
+const RatingControl = ({ data, handleChange, path, label }: RatingControlProps) => (
   <Rating
     value={data}
     updateValue={(newValue: number) => handleChange(path, newValue)}
+    label={label}
   />
 );
 
