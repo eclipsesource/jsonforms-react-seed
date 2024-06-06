6682b4b0e5fdd3ec0d222b4b990db5abc8d8443d
Mayank <mayank.srmu@gmail.com>
2024-06-07 04:47:38 +0530

    rename output directory to build

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build',
+  },
   test: {
     environment: 'jsdom',
     coverage: {
