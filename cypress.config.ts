8650cc94a73a9610d9aa9addbb00b40becc4eed3
Lucas Koehler <lkoehler@eclipsesource.com>
2024-06-11 09:42:17 +0200

    fix eslint error in cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,11 +1,12 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
+      return require('./cypress/plugins/index.js')(on, config);
     },
   },
-})
+});
