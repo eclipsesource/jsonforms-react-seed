b407d09b7c9c5d26cad8014e516cf69c5edc3163
Baroknolia <47528487+Baroknolia@users.noreply.github.com>
2025-06-27 07:23:42 -0700

    Add draggable controls placeholder

--- src/components/DesignerLayout.tsx
+++ src/components/DesignerLayout.tsx
@@ -8,9 +8,14 @@ const DesignerLayout = () => {
   return (
     <div style={{ height: '100vh', display: 'flex', flexDirection: 'column' }}>
       <div style={{ flex: 1 }}>
-        <SplitPane split="vertical" minSize={150} defaultSize={200} className="pane">
+        <SplitPane
+          split="vertical"
+          minSize={150}
+          defaultSize={200}
+          className="pane">
           <Sidebar />
           <SplitPane split="vertical" minSize={200} defaultSize={600}>
+            {/* TODO designate this area as a drop zone for controls */}
             <SchemaTabs />
             <PreviewPane />
           </SplitPane>
