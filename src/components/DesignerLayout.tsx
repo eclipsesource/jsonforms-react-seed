0b00235ecc4bef8e6f254c07f757f6c5e1e7cc1b
Baroknolia <47528487+Baroknolia@users.noreply.github.com>
2025-06-28 04:16:01 -0700

    Add sidebar visibility flag

--- src/components/DesignerLayout.tsx
+++ src/components/DesignerLayout.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import SplitPane from 'react-split-pane-next';
 import Sidebar from './Sidebar';
 import PreviewPane from './PreviewPane';
@@ -5,6 +6,8 @@ import SchemaTabs from './SchemaTabs';
 import StatusBar from './StatusBar';
 
 const DesignerLayout = () => {
+  // TODO implement collapse/expand toggle for sidebar
+  const [sidebarVisible, setSidebarVisible] = useState(true);
   return (
     <div style={{ height: '100vh', display: 'flex', flexDirection: 'column' }}>
       <div style={{ flex: 1 }}>
@@ -13,7 +16,8 @@ const DesignerLayout = () => {
           minSize={150}
           defaultSize={200}
           className="pane">
-          <Sidebar />
+          {sidebarVisible && <Sidebar />}
+          {/* TODO persist sidebar size between sessions */}
           <SplitPane split="vertical" minSize={200} defaultSize={600}>
             {/* TODO designate this area as a drop zone for controls */}
             <SchemaTabs />
